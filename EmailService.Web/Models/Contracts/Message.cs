0e55657237c33ea30a36f45643329489259e1f93
JeroenMBooij <49719495+JeroenMBooij@users.noreply.github.com>
2022-03-08 14:43:29 +0100

    clean up messages

--- EmailService.Web/Models/Contracts/Message.cs
+++ EmailService.Web/Models/Contracts/Message.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Mail;
-using System.Text.RegularExpressions;
 
 namespace EmailService.Web.Models.Contracts
 {
